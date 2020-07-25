2475e4d2ce771e00bc185a21357ce76d8b74367f
daniel2231 <kangheon2231@gmail.com>
2020-07-25 15:05:48 +0900

    first commit

---
+++ router.js
@@ -0,0 +1,49 @@
+let express = require("express");
+let router = express.Router();
+const viewPath = "./views/";
+
+router.get("/", (req, res) => {
+  res.sendFile(viewPath + "index.html", { root: __dirname });
+});
+
+router.get("/restaurant", (req, res) => {
+  res.sendFile(viewPath + "restaurant.html", { root: __dirname });
+});
+
+router.get("/random", (req, res) => {
+  res.sendFile(viewPath + "random.html", { root: __dirname });
+});
+
+router.get("/bar", (req, res) => {
+  res.sendFile(viewPath + "bar.html", { root: __dirname });
+});
+
+let rsData = [
+  {
+    rsid: "정돈",
+  },
+  {
+    rsid: "234",
+  },
+  {
+    rsid: "345",
+  },
+];
+
+let brData = [
+  {
+    brid: "987",
+  },
+];
+
+router.get("/restaurant/:rsid", function (req, res, next) {
+  let rsid = req.params.rsid;
+  res.sendFile(viewPath + `/restaurant/${rsid}.html`, { root: __dirname });
+});
+
+router.get("/bar/:brid", function (req, res, next) {
+  let brid = req.params.brid;
+  res.sendFile(viewPath + `/bar/${brid}.html`, { root: __dirname });
+});
+
+module.exports = router;
