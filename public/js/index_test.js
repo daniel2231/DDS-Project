15e894d088b73e1f57d1d40fa7fc2fcc427c9bf6
June <lovekwon200@gmail.com>
2020-08-03 00:35:34 +0900

    semi_test

--- public/js/index_test.js
+++ public/js/index_test.js
@@ -7,20 +7,21 @@
 
 var dat;
 
+
 var bobImgArr = new Array(); //배열선언
-bobImgArr[0]='../img/메밀향그집.jpg'
-bobImgArr[1]='../img/청춘직화.jpeg'
-bobImgArr[2]='../img/바오밥.jpeg'
-bobImgArr[3]='../img/깔리.jpeg'
-bobImgArr[4]='../img/명륜건강원.jpg'
-bobImgArr[5]='../img/버거파크.jpeg'
-bobImgArr[6]='../img/산쪼메.jpg'
-bobImgArr[7]='../img/소친친.jpg'
-bobImgArr[8]='../img/정돈.jpg'
-bobImgArr[9]='../img/쪼리닭.jpg'
-bobImgArr[10]='../img/파스타마켓.jpeg'
-bobImgArr[11]='../img/혜화칼국수.jpg'
-bobImgArr[12]='../img/호호식당.jpg'
+bobImgArr[0]='../public/img/밥집/메밀향그집.jpg'
+bobImgArr[1]='../public/img/밥집/청춘직화.jpeg'
+bobImgArr[2]='../public/img/밥집/바오밥.jpeg'
+bobImgArr[3]='../public/img/밥집/깔리.jpeg'
+bobImgArr[4]='../public/img/밥집/명륜건강원.jpg'
+bobImgArr[5]='../public/img/밥집/버거파크.jpeg'
+bobImgArr[6]='../public/img/밥집/산쪼메.jpg'
+bobImgArr[7]='../public/img/밥집/소친친.jpg'
+bobImgArr[8]='../public/img/밥집/정돈.jpg'
+bobImgArr[9]='../public/img/밥집/쪼리닭.jpg'
+bobImgArr[10]='../public/img/밥집/파스타마켓.jpeg'
+bobImgArr[11]='../public/img/밥집/혜화칼국수.jpg'
+bobImgArr[12]='../public/img/밥집/호호식당.jpg'
 
 var bobArr = new Array(); //배열선언
 bobArr[0]='메밀향그집'
@@ -36,7 +37,6 @@ bobArr[9]='쪼리닭'
 bobArr[10]='파스타마켓'
 bobArr[11]='혜화칼국수'
 bobArr[12]='호호식당'
-
 
 function toggleImg() {
     dat=Math.floor(Math.random()*10);
